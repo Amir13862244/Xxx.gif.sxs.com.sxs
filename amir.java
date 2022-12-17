f5a4f4651240f166b13afb65202e812b5def9ffc
Amir13862244 <120459185+Amir13862244@users.noreply.github.com>
2022-12-17 22:11:33 +0330

    Create amir.java

---
+++ amir.java
@@ -0,0 +1,10 @@
+while(true){  
+//code to be executed  
+}  
+public class WhileExample2 {  
+public static void main(String[] args) {  
+    while(true){  
+        System.out.println("https://s6.uupload.ir/files/images_(30)_go8v.jpeg");  
+    }  
+}  
+}
